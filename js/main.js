1e7503cee7f06cde1ed96d7d266c0f135d261e3d
Hunter <v.kargalov@gmail.com>
2019-10-05 23:23:27 +0500

    Делает страницу актиной/неактивной, заполняет поля адреса, валидация на количество комнат и мест

--- js/main.js
+++ js/main.js
@@ -15,6 +15,8 @@ var NUMBER_OF_ADVS = 8;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 
+var ENTER_KEYCODE = 13;
+
 // Функция рандомного числа, в том числе в диапазоне
 function getRandomNumbers(fromNumber, toNumber) { // Если передать один аргумент - отдаст число от 0 до x, если два - вернет число в указанном диапазоне
   if (toNumber === undefined) {
@@ -69,10 +71,6 @@ var getRandomAdvs = function (numberOfAdv) {
   return advs;
 };
 
-// Делаем карту активной
-var mapActive = document.querySelector('.map');
-mapActive.classList.remove('map--faded');
-
 // Генерируем пин
 var renderPin = function (adv) {
   var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
@@ -87,8 +85,9 @@ var renderPin = function (adv) {
 };
 
 // Отрисовываем пины
+var pinList = document.querySelector('.map__pins');
+
 var drawPins = function (advsList) {
-  var pinList = document.querySelector('.map__pins');
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < advsList.length; i++) {
@@ -96,8 +95,6 @@ var drawPins = function (advsList) {
   }
   return pinList.appendChild(fragment);
 };
-
-drawPins(getRandomAdvs(NUMBER_OF_ADVS));
 
 // Генерируем карточку
 var renderCard = function (adv) {
@@ -148,4 +145,96 @@ var drawCards = function (advsList) {
   return mapFilters.before(fragment);
 };
 
-drawCards(getRandomAdvs(NUMBER_OF_ADVS));
+// Делаем карту активной
+var mapActive = document.querySelector('.map');
+var mainPin = document.querySelector('.map__pin--main');
+
+// Функция переключения режима полей форм (активный/неактивный)
+function switchActiveForm(boolean) {
+  var formInputsSelectors = document.querySelectorAll('.ad-form fieldset, .map__filters fieldset, .map__filters select');
+  var advForm = document.querySelector('.ad-form');
+  if (boolean === true) {
+    for (var i = 0; i < formInputsSelectors.length; i++) {
+      formInputsSelectors[i].disabled = false;
+      advForm.classList.remove('ad-form--disabled');
+    }
+  } else {
+    for (var j = 0; j < formInputsSelectors.length; j++) {
+      formInputsSelectors[j].disabled = true;
+    }
+  }
+}
+
+switchActiveForm(false);
+
+var mainPinClickHandler = function () {
+  mapActive.classList.remove('map--faded');
+  switchActiveForm(true);
+  drawPins(getRandomAdvs(NUMBER_OF_ADVS));
+  drawCards(getRandomAdvs(NUMBER_OF_ADVS));
+  getMainPinAddress(true);
+  mainPin.removeEventListener('mousedown', mainPinClickHandler);
+  mainPin.removeEventListener('keydown', mainPinEnterPressHandler);
+};
+
+var mainPinEnterPressHandler = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    mainPinClickHandler();
+  }
+};
+
+mainPin.addEventListener('mousedown', mainPinClickHandler);
+mainPin.addEventListener('keydown', mainPinEnterPressHandler);
+
+// Вычисляем и подставяем адресс главного пина
+var MAIN_PIN_WIDTH = 62;
+var MAIN_PIN_HEIGHT = 62;
+var MAIN_PIN_HEIGHT_POINT = 84;
+var pinAddress = document.querySelector('#address');
+
+function getMainPinAddress(isActiveBoolean) {
+  if (isActiveBoolean) {
+    pinAddress.value = (mainPin.getBoundingClientRect().left - pinList.getBoundingClientRect().left + MAIN_PIN_WIDTH / 2) + ', ' + (mainPin.getBoundingClientRect().top - pinList.getBoundingClientRect().top + MAIN_PIN_HEIGHT_POINT / 2);
+  } else {
+    pinAddress.value = (mainPin.getBoundingClientRect().left - pinList.getBoundingClientRect().left + MAIN_PIN_WIDTH / 2) + ', ' + (mainPin.getBoundingClientRect().top - pinList.getBoundingClientRect().top + MAIN_PIN_HEIGHT / 2);
+  }
+}
+
+getMainPinAddress();
+
+// Накладываем валиадацию на зависимость количества комнат от количества гостей и наоборот
+// Объект, в котором ключ - количество комнат, а значение - массив доступного количества места для такого количества комнат
+var capacityRoomsGuests = {
+  1: [1],
+  2: [1, 2],
+  3: [1, 2, 3],
+  100: [0]
+};
+
+var roomNumber = document.querySelector('#room_number');
+var capacityRoomOptions = document.querySelectorAll('#capacity option');
+
+// Функция, которая срабатывает при смене количества комнат. Ограничивает список мест до разрешенного для даннного количества комнат
+function matchRoomsToGuests(evt) {
+  var targetValue = evt.target.value;
+
+  // Делаю неактивными все пункты количества мест
+  for (var i = 0; i < capacityRoomOptions.length; i++) {
+    capacityRoomOptions[i].disabled = true;
+  }
+
+  for (var k = 0; k < Object.keys(capacityRoomsGuests).length; k++) { // Цикл идет по каждому ключу, сравнивая отловленное количество комнат с ключами объекта
+    if (targetValue === String(Object.keys(capacityRoomsGuests)[k])) {
+      for (var l = 0; l < capacityRoomsGuests[Object.keys(capacityRoomsGuests)[k]].length; l++) { // При совпадении с ключом, идет обращением к массиву (значение), к каждому его элементу
+        for (var m = 0; m < capacityRoomOptions.length; m++) { // Сопоставляем спискок количества мест с элементом массива
+          if (capacityRoomOptions[m].value === String(capacityRoomsGuests[Object.keys(capacityRoomsGuests)[k]][l])) { // Если элемент массива найден, соответствующий пункт списка мест становится активным
+            capacityRoomOptions[m].disabled = false;
+            capacityRoomOptions[m].selected = true; // Дополнительно помечаем пункт списка мест как "selected".
+          }
+        }
+      }
+    }
+  }
+}
+
+roomNumber.addEventListener('change', matchRoomsToGuests);
